677933d0e52884be21cd342ae6b0f59cadafb911
Bishal_lamichhane <bishallamichhane4433@gmail.com>
2023-07-07 19:36:29 +0545

    pieces travel in path

---
+++ test.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<windows.h>
+using namespace std;
+int main(){
+    int cord[][4]={{12,55,65,78},{17,88,64,102},{20,55,12,65}};
+    int i=0;
+    for (i=0;i<3;i++){
+        cout<<cord[i][2]<<endl;
+        Sleep(1000);
+    };
+    cout << "HI!" << endl;
+    
+    cout << "Bishal" << endl;
+    return 0;
+};
