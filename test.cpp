6c37510b4cec0fc46baf0ae79cac39af79cdd70b
Bishal_lamichhane <bishallamichhane4433@gmail.com>
2023-07-09 20:40:40 +0545

    random function and dice functions in gray region

--- test.cpp
+++ test.cpp
@@ -1,15 +1,12 @@
 #include<iostream>
 #include<windows.h>
+#include<string>
+#include<time.h>
+#include <cstdlib>
 using namespace std;
 int main(){
-    int cord[][4]={{12,55,65,78},{17,88,64,102},{20,55,12,65}};
-    int i=0;
-    for (i=0;i<3;i++){
-        cout<<cord[i][2]<<endl;
-        Sleep(1000);
-    };
-    cout << "HI!" << endl;
-    
-    cout << "Bishal" << endl;
-    return 0;
+  int i=0;
+  srand(time(0));
+cout << (rand()%6)+1 << " ";
+  return 0;
 };
